fa0814e26a0989ce5dd71dd7db8aa15eddc4c816
mamangoding <indri@pnp.ac.id>
2024-09-06 08:29:04 +0700

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+   Nama file : hello.c
+   Nama pembuat : Indri Rahmayuni
+   Tanggal pembuatan : 6 September 2018
+   Deskripsi :  Menuliskan kata-kata hello world ke layar
+*/
+#include "stdio.h"
+void main()
+{
+    printf("\t\tHello World!!\n");
+    printf("\tProgram bahasa C pertama saya\n");
+    printf("========================================================\n");
+    printf("||\tNama\t: Indri Rahmayuni                     ||\n");
+    printf("||\tNo BP\t: 12345678                            ||\n");
+    printf("||\tProdi\t: Teknologi Rekayasa Perangkat Lunak  ||\n");
+    printf("||\tKelas\t: 1B                                  ||\n");
+    printf("========================================================\n");
+}
+
+
+
